6eba5041260b723368fdb6801b13d0767d697382
luisanegri <luisaanegri@gmail.com>
2020-02-12 13:27:55 +0100

    connect database

--- db.js
+++ db.js
@@ -1,5 +1,7 @@
 const Sequelize = require('sequelize');
-const databaseUrl = 'postgres://postgres:6772@localhost:5432/postgres';
+const databaseUrl =
+  process.env.DATABASE_URL ||
+  'postgres://postgres:6772@localhost:5432/postgres';
 const db = new Sequelize(databaseUrl);
 
 db.sync({ force: false })
